29f62a265a51a7c40926370b0bdf4e0ecc732d95
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-10-14 18:02:30 +0600

    server address updated

--- src/data/apiUrl.ts
+++ src/data/apiUrl.ts
@@ -2,5 +2,5 @@ import { IMAGE_BB_API_KEY } from '@/config';
 
 export const API_URL = {
   IMAGE_BB_URL: `https://api.imgbb.com/1/upload?key=${IMAGE_BB_API_KEY}`,
-  SERVER_ADDRESS: 'https://0203-103-217-111-140.ngrok-free.app',
+  SERVER_ADDRESS: 'https://echo-net-pi.vercel.app',
 };
